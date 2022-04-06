09b6daeec5f373ccb78f4ce884bf20a97664bc50
nicoabb <nicolas.briceno@correo.unimet.edu.ve>
2022-04-05 23:44:26 -0400

    Carousel responsive done

--- PortfolioPage/src/Components/LandPage/Carousel.jsx
+++ PortfolioPage/src/Components/LandPage/Carousel.jsx
@@ -1,13 +1,18 @@
-import styles from "./Carrousel.module.css";
+import styles from "./Carousel.module.css";
 import { useState, useEffect } from "react";
-import img0 from "../../Img/1.jpg";
-import img1 from "../../Img/2.jpg";
-import img2 from "../../Img/3.jpg";
-import img3 from "../../Img/4.jpg";
-import img4 from "../../Img/5.jpg";
+import img1 from "../../Img/1.jpg";
+import img2 from "../../Img/2.jpg";
+import img3 from "../../Img/3.jpg";
+import img4 from "../../Img/4.jpg";
+import img5 from "../../Img/5.jpg";
 
 const Carrousel = () => {
+    /* mainImg is img1, the code in general takes the position of img1 as a reference.
+    When the user clicks on an image, mainImg gets added a value, depending on that
+    value the rest of the images move of position */
+
     let [mainImg, setPosition] = useState(3);
+    // grid is an object with CSS stored
     let [grid1, setGrid1] = useState({ gridColumn: 3 / 5, order: 7 });
     let [grid2, setGrid2] = useState({ gridColumn: 4 / 6, order: 5 });
     let [grid3, setGrid3] = useState({ gridColumn: 5 / 7, order: 3 });
@@ -25,6 +30,7 @@ const Carrousel = () => {
         return num;
     };
 
+    // this is where grid positions are updated
     const updateStyle = (spot) => {
         let ord = 1;
         if (spot === "3 / 5") {
@@ -41,6 +47,8 @@ const Carrousel = () => {
         return grid;
     };
 
+    /* p stands for position, the position of the img (p1 for img1, p2 for img2...) 
+    spot is position, but in grid-columns, imagine p1 is 3, then spot1 is 3 / 5 */
     const moveGrid = (spots) => {
         setPosition(checkNum(mainImg));
         let p1 = mainImg;
@@ -66,30 +74,38 @@ const Carrousel = () => {
         setGrid5(updateStyle(spots[4]));
     }, [mainImg]);
 
+    /* TIMER LOGIC */
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setPosition(mainImg + 1);
+        }, 7000);
+        return () => clearInterval(interval);
+    }, [mainImg]);
+
     return (
         <section className={styles.container}>
             <div className={styles.carrousel}>
-                <img id={styles["p1"]} src={img0} alt="Photo 1" style={grid1} />
+                <img id={styles["p1"]} src={img1} alt="Photo 1" style={grid1} />
                 <span
                     className={styles.p2Layer}
-                    onClick={() => setPosition(mainImg + 1)}
+                    onClick={() => setPosition(mainImg - 1)}
                 ></span>
-                <img id={styles["p2"]} src={img1} alt="Photo 2" style={grid2} />
+                <img id={styles["p2"]} src={img2} alt="Photo 2" style={grid2} />
                 <span
                     className={styles.p5Layer}
-                    onClick={() => setPosition(mainImg - 1)}
-                ></span>
-                <img id={styles["p5"]} src={img4} alt="Photo 5" style={grid5} />
-                <span
-                    className={styles.p3Layer}
                     onClick={() => setPosition(mainImg + 1)}
                 ></span>
-                <img id={styles["p3"]} src={img2} alt="Photo 3" style={grid3} />
+                <img id={styles["p5"]} src={img5} alt="Photo 5" style={grid5} />
+                <span
+                    className={styles.p3Layer}
+                    onClick={() => setPosition(mainImg - 2)}
+                ></span>
+                <img id={styles["p3"]} src={img3} alt="Photo 3" style={grid3} />
                 <span
                     className={styles.p4Layer}
-                    onClick={() => setPosition(mainImg - 1)}
+                    onClick={() => setPosition(mainImg + 2)}
                 ></span>
-                <img id={styles["p4"]} src={img3} alt="Photo 4" style={grid4} />
+                <img id={styles["p4"]} src={img4} alt="Photo 4" style={grid4} />
             </div>
             <div className={styles.line}></div>
         </section>
--- PortfolioPage/src/Pages/LandPage.jsx
+++ PortfolioPage/src/Pages/LandPage.jsx
@@ -1,11 +1,11 @@
-import Carrousel from "../Components/LandPage/Carrousel";
+import Carousel from "../Components/LandPage/Carousel";
 import IntroText from "../Components/LandPage/IntroText";
 import ProjectsPrev from "../Components/LandPage/ProjectsPrev";
 
 const LandPage = () => {
     return (
         <div>
-            <Carrousel />
+            <Carousel />
             <IntroText />
             <ProjectsPrev />
         </div>
