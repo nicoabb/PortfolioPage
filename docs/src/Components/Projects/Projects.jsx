55891190832a85a8fdc8e59ca207b4177774e513
Nicolas Briceño <nicolas.briceno@correo.unimet.edu.ve>
2024-02-02 12:22:47 -0400

    Update Projects page

--- docs/src/Components/Projects/Projects.jsx
+++ docs/src/Components/Projects/Projects.jsx
@@ -1,51 +1,44 @@
 import styles from "./Projects.module.css";
 import PCards from "./pCards";
-import img1 from "../../Img/project1.png";
-import img2 from "../../Img/project2.png";
-import img3 from "../../Img/project3.png";
-import { AiFillHtml5 } from "react-icons/ai";
-import { FaCss3, FaReact } from "react-icons/fa";
-import { SiJavascript } from "react-icons/si";
+import img1 from "../../Img/1.png";
+import img2 from "../../Img/2.png";
+import img3 from "../../Img/3.png";
+import img4 from "../../Img/4.png";
+import img5 from "../../Img/5.png";
 import { useTranslation } from "react-i18next";
 
 const Projects = (props) => {
     const [t, i18n] = useTranslation("global");
+    const projects = [
+        {image: img1, name: "Triveneco", desc: t("projects.firstDesc")},
+        {image: img2, name: "Triveneco", desc: t("projects.secondDesc")},
+        {image: img3, name: "Triveneco", desc: t("projects.secondDesc")},
+        {image: img4, name: "Triveneco", desc: t("projects.secondDesc")},
+        {image: img5, name: "Triveneco", desc: t("projects.secondDesc")}
+    ];
 
     return (
         <section className={styles.proj}>
             <h1>{t("projects.title")}</h1>
-            <PCards
-                pLink="https://triveneco.web.app/"
-                img={img1}
-                htmlLogo={<AiFillHtml5 />}
-                cssLogo={<FaCss3 />}
-                jsLogo={<SiJavascript />}
-                reactLogo={<FaReact />}
-                name="Triveneco"
-                desc={t("projects.firstDesc")}
-            ></PCards>
-            <PCards
-                pLink="https://nicoabb.github.io/Random-page/"
-                img={img3}
-                htmlLogo={<AiFillHtml5 />}
-                cssLogo={<FaCss3 />}
-                jsLogo={null}
-                reactLogo={null}
-                name="Random Page"
-                desc={t("projects.secondDesc")}
-            ></PCards>
-            <PCards
-                pLink="https://nicoabb.github.io/iOS-Calculator/"
-                img={img2}
-                htmlLogo={<AiFillHtml5 />}
-                cssLogo={<FaCss3 />}
-                jsLogo={<SiJavascript />}
-                reactLogo={null}
-                name="iOS Calculator"
-                desc={t("projects.thirdDesc")}
-            ></PCards>
+            {projects.map((project, index) => (
+                <div 
+                    className={styles.cardBox} 
+                    style={{
+                        backgroundColor: index % 2 === 0 ? "var(--primary-color-200)" : "var(--primary-color-150)"
+                    }}
+                >
+                    <PCards
+                        image={project.image}
+                        name={project.name}
+                        desc={project.desc}
+                        view={t("projects.view")}
+                        extraStyle={{flexDirection: index % 2 === 0 ? "row-reverse" : "row"}}
+                    ></PCards>
+                </div>
+            ))}
         </section>
     );
 };
+
 
 export default Projects;
--- docs/src/Components/Projects/pCards.jsx
+++ docs/src/Components/Projects/pCards.jsx
@@ -3,21 +3,24 @@ import styles from "./pCards.module.css";
 
 const PCards = (props) => {
     return (
-        <div className={styles.pCard}>
-            <div className={`${styles.circle} ${styles.c1}`}></div>
-            <div className={styles.rectangle}></div>
-            <a href={props.pLink} target="_blank" rel="noopener noreferrer">
-                <img src={props.img} alt="Project image" />
-            </a>
-            <div className={styles.icons}>
-                {props.htmlLogo}
-                {props.cssLogo}
-                {props.jsLogo}
-                {props.reactLogo}
+        <div className={styles.pCard} style={props.extraStyle}>
+            <div className={styles.imgBox}>
+                <img src={props.image} alt={props.imgAlt} />
             </div>
-            <h3>{props.name}</h3>
-            <p>{props.desc}</p>
-            <div className={`${styles.circle} ${styles.c2}`}></div>
+            <div className={styles.textBox}>
+                <h3>{props.name}</h3>
+                <p>{props.desc}</p>
+                <div className={styles.btnBox}>
+                    <a  
+                            href={props.url}
+                            target="_blank"
+                            rel="noreferrer"
+                    >
+                        <button>{props.view}</button>
+                    </a>
+                    {props.web}
+                </div>
+            </div>
         </div>
     );
 };
