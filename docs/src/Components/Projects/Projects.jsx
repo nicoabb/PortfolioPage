6689d6f1d4c19258432fbc63c6a62b38a4715e1e
Nicolas Briceño <nicolas.briceno@correo.unimet.edu.ve>
2024-02-13 17:22:28 -0400

    Projects Responsive Design

--- docs/src/Components/Projects/Projects.jsx
+++ docs/src/Components/Projects/Projects.jsx
@@ -6,9 +6,11 @@ import img3 from "../../Img/3.png";
 import img4 from "../../Img/4.png";
 import img5 from "../../Img/5.png";
 import { useTranslation } from "react-i18next";
+import { useMediaQuery } from 'react-responsive';
 
 const Projects = (props) => {
     const [t, i18n] = useTranslation("global");
+    const isMobile = useMediaQuery({ query: '(max-width: 719px)' });
     const projects = [
         {image: img1, name: t("projects.oneTitle"), desc: t("projects.oneDesc"), url: "https://github.com/nicoabb/F4_Front_Tesis", view: t("projects.viewf"), extraView: t("projects.viewb"), secondButton: true, extraUrl: "https://github.com/nicoabb/F4_Back_Tesis"},
         {image: img2, name: t("projects.twoTitle"), desc: t("projects.twoDesc"), url: "https://github.com/nicoabb/Proyecto1_EDD", view: t("projects.view"), secondButton: false},
@@ -22,7 +24,7 @@ const Projects = (props) => {
             <h1>{t("projects.title")}</h1>
             {projects.map((project, index) => (
                 <div 
-                    id={`p${index}`}
+                    key={`p${index}`}
                     className={styles.cardBox} 
                     style={{
                         backgroundColor: index % 2 === 0 ? "var(--primary-color-200)" : "var(--primary-color-150)"
@@ -34,7 +36,7 @@ const Projects = (props) => {
                         desc={project.desc}
                         url={project.url}
                         view={project.view}
-                        extraStyle={{flexDirection: index % 2 === 0 ? "row" : "row-reverse"}}
+                        extraStyle={{flexDirection: isMobile ? "column" : (index % 2 === 0 ? "row" : "row-reverse")}}
                         secondBtn={ project.secondButton && 
                             <>
                                 <a 
